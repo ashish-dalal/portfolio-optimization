913f1bcf593fa672cd13a31b0c391f419a9916ad
Ashish Dalal <ashish.dalal.cd.che@itbhu.ac.in>
2024-05-12 19:12:55 +0530

    added performance ratios, commodities and nifty50 stocks

--- basic-tasks/functions.py
+++ basic-tasks/functions.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+from tqdm import tqdm
 
+"""
 ## A FUNCTION TO RETRIEVE INFORMATION FOR A GIVEN LIST OF STOCKS
 def fetch_data(stock_list, start_date, end_date):
     
@@ -12,8 +14,60 @@ def fetch_data(stock_list, start_date, end_date):
         stock_list[i] = stock_list[i].upper()
 
     stock_data = {}
-    for stock in stock_list:
-        ticker = yf.Ticker(stock)
-        stock_data[stock] = ticker.history(start=start_date, end=end_date)['Close']
+    for i in tqdm(range(len(stock_list))):
+        ticker = yf.Ticker(stock_list[i])
+        stock_data[stock_list[i]] = ticker.history(start=start_date, end=end_date)['Close']
         
     return pd.DataFrame(stock_data)
+    """
+
+    ## A FUNCTION TO RETRIEVE INFORMATION FOR A GIVEN LIST OF STOCKS
+
+def fetch_data(stock_list, start_date, end_date, div=True):
+    
+    """Returns stock_price_df, stock_dividend_df"""
+    
+    if type(stock_list)!=list:
+        stock_list = [stock_list]
+    
+    for i in range(len(stock_list)):
+        stock_list[i] = stock_list[i].upper()
+
+    stock_price = {}
+    stock_dividend = {}
+    for i in tqdm(range(len(stock_list))):
+        ticker = yf.Ticker(stock_list[i])
+        stock_data = ticker.history(start=start_date, end=end_date)
+        stock_price[stock_list[i]] = stock_data['Close']
+        stock_dividend[stock_list[i]] = stock_data['Dividends']
+        
+    return pd.DataFrame(stock_price), pd.DataFrame(stock_dividend)
+
+## FUNCTION TO CONVERT WEIGHTS TO DATAFRAME
+
+def weights_to_dataframe(arr, cols):
+    
+    if arr.ndim != 2:
+        raise ValueError("Input error must have only 2 dimensions")
+    
+    for i in range(arr.shape[1]):
+        if i==0:
+            df = pd.DataFrame(arr[:,i])
+        else:
+            temp = pd.DataFrame(arr[:,i])
+            df = pd.concat([df,temp], axis=1)
+    df.columns = cols
+    return df
+
+## TO CALCULATE BETA FOR A STOCK
+
+def calculate_beta(log_return_portfolio, log_return_market):
+    
+    covariance = np.cov(log_return_portfolio, log_return_market.iloc[:,0])[0,1]
+    variance_market = log_return_market.var()
+    
+    beta = covariance/variance_market
+    
+    return beta[0]
+
+    
